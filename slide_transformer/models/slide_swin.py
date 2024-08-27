e876c873d2aa9ba9fc043acb96b633c5ff5379b6
Tianzhu Ye (FESCO Adecco Human Resources) <v-tianzhuye@microsoft.com>
2024-08-27 10:05:42 +0800

    finish code

--- slide_transformer/models/slide_swin.py
+++ slide_transformer/models/slide_swin.py
@@ -1,14 +1,3 @@
-# --------------------------------------------------------
-# Swin Transformer
-# Copyright (c) 2021 Microsoft
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Ze Liu
-# --------------------------------------------------------
-# Agent Attention: On the Integration of Softmax and Linear Attention
-# Modified by Dongchen Han
-# -----------------------------------------------------------------------
-
-
 import torch
 import torch.nn as nn
 import torch.utils.checkpoint as checkpoint
@@ -65,29 +54,6 @@ def window_reverse(windows, window_size, H, W):
     x = windows.view(B, H // window_size, W // window_size, window_size, window_size, -1)
     x = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(B, H, W, -1)
     return x
-
-
-class PosCNN(nn.Module):
-    def __init__(self, in_chans, embed_dim=768, s=1):
-        super(PosCNN, self).__init__()
-        self.proj = nn.Sequential(nn.Conv2d(in_chans, embed_dim, 3, s, 1, bias=True, groups=embed_dim), )
-        self.s = s
-
-    def forward(self, x):
-        B, N, C = x.shape
-        H = int(N ** 0.5)
-        W = int(N ** 0.5)
-        feat_token = x
-        cnn_feat = feat_token.transpose(1, 2).view(B, C, H, W)
-        if self.s == 1:
-            x = self.proj(cnn_feat) + cnn_feat
-        else:
-            x = self.proj(cnn_feat)
-        x = x.flatten(2).transpose(1, 2)
-        return x
-
-    def no_weight_decay(self):
-        return ['proj.%d.weight' % i for i in range(4)]
 
 
 class WindowAttention(nn.Module):
@@ -174,126 +140,67 @@ class WindowAttention(nn.Module):
     def extra_repr(self) -> str:
         return f'dim={self.dim}, window_size={self.window_size}, num_heads={self.num_heads}'
 
-    def flops(self, N):
-        # calculate flops for 1 window with token length of N
-        flops = 0
-        # qkv = self.qkv(x)
-        flops += N * self.dim * 3 * self.dim
-        # attn = (q @ k.transpose(-2, -1))
-        flops += self.num_heads * N * (self.dim // self.num_heads) * N
-        #  x = (attn @ v)
-        flops += self.num_heads * N * N * (self.dim // self.num_heads)
-        # x = self.proj(x)
-        flops += N * self.dim * self.dim
-        return flops
 
-
-class AgentAttention(nn.Module):
-    r""" Window based multi-head self attention (W-MSA) module with relative position bias.
-    It supports both of shifted and non-shifted window.
-
-    Args:
-        dim (int): Number of input channels.
-        num_heads (int): Number of attention heads.
-        qkv_bias (bool, optional):  If True, add a learnable bias to query, key, value. Default: True
-        qk_scale (float | None, optional): Override default qk scale of head_dim ** -0.5 if set
-        attn_drop (float, optional): Dropout ratio of attention weight. Default: 0.0
-        proj_drop (float, optional): Dropout ratio of output. Default: 0.0
-    """
-
-    def __init__(self, dim, window_size, num_heads, qkv_bias=True, qk_scale=None, attn_drop=0., proj_drop=0.,
-                 shift_size=0, agent_num=49, **kwargs):
+class SlideAttention(nn.Module):
+    def __init__(
+        self, input_resolution, dim, num_heads, ka, qkv_bias=True, qk_scale=None, attn_drop=0., proj_drop=0., padding_mode='zeros'):
 
         super().__init__()
+        self.input_resolution = input_resolution
         self.dim = dim
-        self.window_size = window_size  # Wh, Ww
         self.num_heads = num_heads
-        head_dim = dim // num_heads
-        self.scale = head_dim ** -0.5
+        self.head_dim = dim // num_heads
+        self.scale = qk_scale or self.head_dim ** -0.5
+        self.padding_mode = padding_mode
+        self.ka = ka
         self.qkv = nn.Linear(dim, dim * 3, bias=qkv_bias)
         self.attn_drop = nn.Dropout(attn_drop)
         self.proj = nn.Linear(dim, dim)
         self.proj_drop = nn.Dropout(proj_drop)
-        self.softmax = nn.Softmax(dim=-1)
-        self.shift_size = shift_size
 
-        self.agent_num = agent_num
-        self.dwc = nn.Conv2d(in_channels=dim, out_channels=dim, kernel_size=(3, 3), padding=1, groups=dim)
-        self.an_bias = nn.Parameter(torch.zeros(num_heads, agent_num, 7, 7))
-        self.na_bias = nn.Parameter(torch.zeros(num_heads, agent_num, 7, 7))
-        self.ah_bias = nn.Parameter(torch.zeros(1, num_heads, agent_num, window_size[0], 1))
-        self.aw_bias = nn.Parameter(torch.zeros(1, num_heads, agent_num, 1, window_size[1]))
-        self.ha_bias = nn.Parameter(torch.zeros(1, num_heads, window_size[0], 1, agent_num))
-        self.wa_bias = nn.Parameter(torch.zeros(1, num_heads, 1, window_size[1], agent_num))
-        trunc_normal_(self.an_bias, std=.02)
-        trunc_normal_(self.na_bias, std=.02)
-        trunc_normal_(self.ah_bias, std=.02)
-        trunc_normal_(self.aw_bias, std=.02)
-        trunc_normal_(self.ha_bias, std=.02)
-        trunc_normal_(self.wa_bias, std=.02)
-        pool_size = int(agent_num ** 0.5)
-        self.pool = nn.AdaptiveAvgPool2d(output_size=(pool_size, pool_size))
+        self.dep_conv = nn.Conv2d(self.head_dim, self.ka*self.ka*self.head_dim, kernel_size=self.ka, bias=True, groups=self.head_dim, padding=self.ka//2, padding_mode=padding_mode)
+        self.dep_conv1 = nn.Conv2d(self.head_dim, self.ka*self.ka*self.head_dim, kernel_size=self.ka, bias=True, groups=self.head_dim, padding=self.ka//2, padding_mode=padding_mode)
+
+        self.reset_parameters()
+        
+        self.relative_position_bias_table = nn.Parameter(torch.zeros(1, self.num_heads, 1, self.ka*self.ka, 1, 1))
+        trunc_normal_(self.relative_position_bias_table, std=.02)
+        self.softmax = nn.Softmax(dim=3)
+
+    def reset_parameters(self):
+        # shift initialization for group convolution
+        kernel = torch.zeros(self.ka*self.ka, self.ka, self.ka)
+        for i in range(self.ka*self.ka):
+            kernel[i, i//self.ka, i%self.ka] = 1.
+        kernel = kernel.unsqueeze(1).repeat(self.head_dim, 1, 1, 1)
+        self.dep_conv.weight = nn.Parameter(data=kernel, requires_grad=False)
 
     def forward(self, x, mask=None):
-        """
-        Args:
-            x: input features with shape of (num_windows*B, N, C)
-            mask: (0/-inf) mask with shape of (num_windows, Wh*Ww, Wh*Ww) or None
-        """
-        b, n, c = x.shape
-        h = int(n ** 0.5)
-        w = int(n ** 0.5)
-        num_heads = self.num_heads
-        head_dim = c // num_heads
-        qkv = self.qkv(x).reshape(b, n, 3, c).permute(2, 0, 1, 3)
-        q, k, v = qkv[0], qkv[1], qkv[2]  # make torchscript happy (cannot use tensor as tuple)
-        # q, k, v: b, n, c
+        B, L, C = x.shape
+        H, W = self.input_resolution
+        x = x.view(B, H, W, C)
+        qkv = self.qkv(x)
 
-        agent_tokens = self.pool(q.reshape(b, h, w, c).permute(0, 3, 1, 2)).reshape(b, c, -1).permute(0, 2, 1)
-        q = q.reshape(b, n, num_heads, head_dim).permute(0, 2, 1, 3)
-        k = k.reshape(b, n, num_heads, head_dim).permute(0, 2, 1, 3)
-        v = v.reshape(b, n, num_heads, head_dim).permute(0, 2, 1, 3)
-        agent_tokens = agent_tokens.reshape(b, self.agent_num, num_heads, head_dim).permute(0, 2, 1, 3)
+        f_conv = qkv.permute(0, 3, 1, 2).reshape(B*self.num_heads, 3*self.head_dim, H, W)
 
-        position_bias1 = nn.functional.interpolate(self.an_bias, size=self.window_size, mode='bilinear')
-        position_bias1 = position_bias1.reshape(1, num_heads, self.agent_num, -1).repeat(b, 1, 1, 1)
-        position_bias2 = (self.ah_bias + self.aw_bias).reshape(1, num_heads, self.agent_num, -1).repeat(b, 1, 1, 1)
-        position_bias = position_bias1 + position_bias2
-        agent_attn = self.softmax((agent_tokens * self.scale) @ k.transpose(-2, -1) + position_bias)
-        agent_attn = self.attn_drop(agent_attn)
-        agent_v = agent_attn @ v
+        q = (f_conv[:, :self.head_dim, :, :] * self.scale).reshape(B, self.num_heads, self.head_dim, 1, H, W)
+        k = f_conv[:, self.head_dim:2*self.head_dim, :, :]
+        v = f_conv[:, 2*self.head_dim:, :, :]
 
-        agent_bias1 = nn.functional.interpolate(self.na_bias, size=self.window_size, mode='bilinear')
-        agent_bias1 = agent_bias1.reshape(1, num_heads, self.agent_num, -1).permute(0, 1, 3, 2).repeat(b, 1, 1, 1)
-        agent_bias2 = (self.ha_bias + self.wa_bias).reshape(1, num_heads, -1, self.agent_num).repeat(b, 1, 1, 1)
-        agent_bias = agent_bias1 + agent_bias2
-        q_attn = self.softmax((q * self.scale) @ agent_tokens.transpose(-2, -1) + agent_bias)
-        q_attn = self.attn_drop(q_attn)
-        x = q_attn @ agent_v
+        k = (self.dep_conv(k) + self.dep_conv1(k)).reshape(B, self.num_heads, self.head_dim, self.ka*self.ka, H, W)
+        v = (self.dep_conv(v) + self.dep_conv1(v)).reshape(B, self.num_heads, self.head_dim, self.ka*self.ka, H, W)
 
-        x = x.transpose(1, 2).reshape(b, n, c)
-        v = v.transpose(1, 2).reshape(b, h, w, c).permute(0, 3, 1, 2)
-        x = x + self.dwc(v).permute(0, 2, 3, 1).reshape(b, n, c)
+        k = k + self.relative_position_bias_table
+        
+        attn = (q * k).sum(2, keepdim=True) # B, self.nhead, 1, k^2, H, W
 
+        attn = self.softmax(attn)
+        attn = self.attn_drop(attn)
+
+        x = (attn * v).sum(3).reshape(B, C, H, W).permute(0, 2, 3, 1).view(B, L, C)
         x = self.proj(x)
         x = self.proj_drop(x)
         return x
-
-    def extra_repr(self) -> str:
-        return f'dim={self.dim}, window_size={self.window_size}, num_heads={self.num_heads}'
-
-    def flops(self, N):
-        # calculate flops for 1 window with token length of N
-        flops = 0
-        # qkv = self.qkv(x)
-        flops += N * self.dim * 3 * self.dim
-        # attn = (q @ k.transpose(-2, -1))
-        flops += self.num_heads * N * (self.dim // self.num_heads) * N
-        #  x = (attn @ v)
-        flops += self.num_heads * N * N * (self.dim // self.num_heads)
-        # x = self.proj(x)
-        flops += N * self.dim * self.dim
-        return flops
 
 
 class SwinTransformerBlock(nn.Module):
@@ -318,7 +225,7 @@ class SwinTransformerBlock(nn.Module):
     def __init__(self, dim, input_resolution, num_heads, window_size=7, shift_size=0,
                  mlp_ratio=4., qkv_bias=True, qk_scale=None, drop=0., attn_drop=0., drop_path=0.,
                  act_layer=nn.GELU, norm_layer=nn.LayerNorm,
-                 agent_num=49, attn_type='A'):
+                 ka=3, is_slide=True):
         super().__init__()
         self.dim = dim
         self.input_resolution = input_resolution
@@ -333,11 +240,11 @@ class SwinTransformerBlock(nn.Module):
         assert 0 <= self.shift_size < self.window_size, "shift_size must in 0-window_size"
 
         self.norm1 = norm_layer(dim)
-        assert attn_type in ['A', 'B']
-        if attn_type == 'A':
-            self.attn = AgentAttention(dim=dim, window_size=to_2tuple(self.window_size), num_heads=num_heads,
-                                       qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop, proj_drop=drop,
-                                       agent_num=agent_num)
+
+        if is_slide:
+            self.attn = SlideAttention(input_resolution=input_resolution, 
+                                       dim=dim, num_heads=num_heads, ka=ka,
+                                        qkv_bias=qkv_bias, qk_scale=qk_scale, attn_drop=attn_drop,proj_drop=drop)
         else:
             self.attn = WindowAttention(
                 dim, window_size=to_2tuple(self.window_size), num_heads=num_heads,
@@ -412,24 +319,6 @@ class SwinTransformerBlock(nn.Module):
 
         return x
 
-    def extra_repr(self) -> str:
-        return f"dim={self.dim}, input_resolution={self.input_resolution}, num_heads={self.num_heads}, " \
-               f"window_size={self.window_size}, shift_size={self.shift_size}, mlp_ratio={self.mlp_ratio}"
-
-    def flops(self):
-        flops = 0
-        H, W = self.input_resolution
-        # norm1
-        flops += self.dim * H * W
-        # W-MSA/SW-MSA
-        nW = H * W / self.window_size / self.window_size
-        flops += nW * self.attn.flops(self.window_size * self.window_size)
-        # mlp
-        flops += 2 * H * W * self.dim * self.dim * self.mlp_ratio
-        # norm2
-        flops += self.dim * H * W
-        return flops
-
 
 class PatchMerging(nn.Module):
     r""" Patch Merging Layer.
@@ -473,12 +362,6 @@ class PatchMerging(nn.Module):
     def extra_repr(self) -> str:
         return f"input_resolution={self.input_resolution}, dim={self.dim}"
 
-    def flops(self):
-        H, W = self.input_resolution
-        flops = H * W * self.dim
-        flops += (H // 2) * (W // 2) * 4 * self.dim * 2 * self.dim
-        return flops
-
 
 class BasicLayer(nn.Module):
     """ A basic Swin Transformer layer for one stage.
@@ -503,7 +386,7 @@ class BasicLayer(nn.Module):
     def __init__(self, dim, input_resolution, depth, num_heads, window_size,
                  mlp_ratio=4., qkv_bias=True, qk_scale=None, drop=0., attn_drop=0.,
                  drop_path=0., norm_layer=nn.LayerNorm, downsample=None, use_checkpoint=False,
-                 agent_num=49, attn_type='A'):
+                 ka=3, is_slide=True):
 
         super().__init__()
         self.dim = dim
@@ -512,8 +395,7 @@ class BasicLayer(nn.Module):
         self.use_checkpoint = use_checkpoint
 
         # build blocks
-        attn_types = [(attn_type if attn_type[0] != 'M' else ('A' if i < int(attn_type[1:]) else 'B')) for i in range(depth)]
-        window_sizes = [(window_size if attn_types[i] == 'A' else max(7, (window_size // 8))) for i in range(depth)]
+        window_sizes = [window_size if not is_slide else input_resolution[0] for i in range(depth)]
         self.blocks = nn.ModuleList([
             SwinTransformerBlock(dim=dim, input_resolution=input_resolution,
                                  num_heads=num_heads, window_size=window_sizes[i],
@@ -523,8 +405,8 @@ class BasicLayer(nn.Module):
                                  drop=drop, attn_drop=attn_drop,
                                  drop_path=drop_path[i] if isinstance(drop_path, list) else drop_path,
                                  norm_layer=norm_layer,
-                                 agent_num=agent_num,
-                                 attn_type=attn_types[i])
+                                 ka=ka,
+                                 is_slide=is_slide)
             for i in range(depth)])
 
         # patch merging layer
@@ -543,16 +425,6 @@ class BasicLayer(nn.Module):
             x = self.downsample(x)
         return x
 
-    def extra_repr(self) -> str:
-        return f"dim={self.dim}, input_resolution={self.input_resolution}, depth={self.depth}"
-
-    def flops(self):
-        flops = 0
-        for blk in self.blocks:
-            flops += blk.flops()
-        if self.downsample is not None:
-            flops += self.downsample.flops()
-        return flops
 
 
 class PatchEmbed(nn.Module):
@@ -595,15 +467,8 @@ class PatchEmbed(nn.Module):
             x = self.norm(x)
         return x
 
-    def flops(self):
-        Ho, Wo = self.patches_resolution
-        flops = Ho * Wo * self.embed_dim * self.in_chans * (self.patch_size[0] * self.patch_size[1])
-        if self.norm is not None:
-            flops += Ho * Wo * self.embed_dim
-        return flops
 
-
-class AgentSwinTransformer(nn.Module):
+class SlideSwinTransformer(nn.Module):
     r""" Swin Transformer
         A PyTorch impl of : `Swin Transformer: Hierarchical Vision Transformer using Shifted Windows`  -
           https://arxiv.org/pdf/2103.14030
@@ -635,7 +500,7 @@ class AgentSwinTransformer(nn.Module):
                  drop_rate=0., attn_drop_rate=0., drop_path_rate=0.1,
                  norm_layer=nn.LayerNorm, ape=False, patch_norm=True,
                  use_checkpoint=False,
-                 agent_num=[9, 16, 49, 49], attn_type='BBBB', **kwargs):
+                 ka=3, num_slide_stages=2, **kwargs):
         super().__init__()
         self.num_classes = num_classes
         self.num_layers = len(depths)
@@ -679,8 +544,8 @@ class AgentSwinTransformer(nn.Module):
                                norm_layer=norm_layer,
                                downsample=PatchMerging if (i_layer < self.num_layers - 1) else None,
                                use_checkpoint=use_checkpoint,
-                               agent_num=int(agent_num[i_layer]),
-                               attn_type=attn_type[i_layer] + (attn_type[self.num_layers:] if attn_type[i_layer] == 'M' else ''))
+                               ka=ka,
+                               is_slide=(i_layer < num_slide_stages))
             self.layers.append(layer)
 
         self.norm = norm_layer(self.num_features)
@@ -724,12 +589,3 @@ class AgentSwinTransformer(nn.Module):
         x = self.forward_features(x)
         x = self.head(x)
         return x
-
-    def flops(self):
-        flops = 0
-        flops += self.patch_embed.flops()
-        for i, layer in enumerate(self.layers):
-            flops += layer.flops()
-        flops += self.num_features * self.patches_resolution[0] * self.patches_resolution[1] // (2 ** self.num_layers)
-        flops += self.num_features * self.num_classes
-        return flops
